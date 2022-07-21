1b5542f987627b2d962e84b897c38a89327c7633
低调清华人 <2415674048@qq.com>
2022-07-21 23:02:29 +0800

    完成个人中心功能的开发

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,11 @@
-$(function() {
+$(function () {
     // 调用getUserInfo,获取用户基本信息
     getUserInfo();
 
     // 点击按钮实现退出功能
-    $('#btnLogout').on('click', function() {
+    $('#btnLogout').on('click', function () {
         // 提示用户是否确认退出
-        layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function(index) {
+        layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function (index) {
             // 1.清空本地存储中的token
             localStorage.removeItem('token');
             // 2.重新跳转到登录页面
@@ -24,11 +24,10 @@ function getUserInfo() {
         // headers: {
         //     Authorization: localStorage.getItem('token') || ''
         // },
-        success: function(res) {
+        success: function (res) {
             if (res.status !== 0) {
                 return layui.layer.msg('获取用户信息失败！')
             }
-
             // 调用renderAvatar渲染用户头像
             renderAvatar(res.data);
         },
@@ -47,7 +46,6 @@ function getUserInfo() {
 }
 
 
-
 // 渲染用户头像
 function renderAvatar(user) {
     // 获取用户的名称
@@ -57,7 +55,7 @@ function renderAvatar(user) {
     // 按需渲染用户头像
     if (user.user_pic !== null) {
         // 渲染图片头像
-        $('.layui-nav-img').src('user.user_pic').show();
+        $('.layui-nav-img').attr('src', user.user_pic).show();
         $('.text-avatar').hide();
     } else {
         // 渲染文本头像
